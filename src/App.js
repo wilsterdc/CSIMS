76c792fc2f851c57ebac486b06da18fbb5901626
wilsterdc <delacruz.wilster15@gmail.com>
2023-02-16 14:46:17 +0800

    :rocket: initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from './components/Login'
+
+function App() {
+  
+  return (
+    <div className = 'container'>
+      <Login />
+    </div>
+)}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <form>
+                <div>
+                    <label>Username</label>
+                    <br />
+                    <input type = 'text' placeholder = 'Username'></input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <br />
+                    <input type = 'password' placeholder = 'Password'></input>
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Header
