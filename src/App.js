2a3f0ae1c84ec87e26442c57b276be655202bb66
wilsterdc <delacruz.wilster15@gmail.com>
2023-02-18 23:42:37 +0800

    :rocket: initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <div className = 'container'>
-        <Login />  
+        <Login />
       </div>
     </div>
 )}
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,25 +4,22 @@ const Login = () => {
   return (
     <div>
         <div className = 'cover'>
-            <h2 className = 'head'>CSIMS</h2>
+            <h2>CSIMS</h2>
             <form>
                 <div>
-                    <label className = 'label'>Username</label>
-                    <br />
-                    <input type = 'text' placeholder = 'Username'></input>
+                    <input className = 'login-input' type = 'text' placeholder = 'Username'></input>
                 </div>
                 <div>
-                    <label className = 'label'>Password</label>
-                    <br />
-                    <input type = 'password' placeholder = 'Password'></input>
+                    <input className = 'login-input' type = 'password' placeholder = 'Password'></input>
                 </div>
-                <br />
                 <div>
-                    <p className = 'reset'>Forgot password? Please contact the Office of the Registrar 
+                    <p id = 'failed'>Login failed! Incorrect username or password.</p>
+                </div>
+                <div>
+                    <p className = 'reset'>Forgot password? <br />Please contact the Office of the Registrar 
                         through the following contact number: <em>(044) 769 2021</em> or
-                         through email; <a href = "mailto:inquiries@collegeofmaryimmaculate.edu.ph" target="_blank">
+                         through email; <a href = "mailto:inquiries@collegeofmaryimmaculate.edu.ph" target = 'blank'>
                             inquiries@collegeofmaryimmaculate.edu.ph</a>
-
                     </p>
                 </div>
                 <div>
