1900f3c0b7dc4f41591bccefa0ccd196ed647d0a
wilsterdc <delacruz.wilster15@gmail.com>
2023-02-18 21:31:29 +0800

    :rocket: initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,6 +16,15 @@ const Login = () => {
                     <br />
                     <input type = 'password' placeholder = 'Password'></input>
                 </div>
+                <br />
+                <div>
+                    <p className = 'reset'>Forgot password? Please contact the Office of the Registrar 
+                        through the following contact number: <em>(044) 769 2021</em> or
+                         through email; <a href = "mailto:inquiries@collegeofmaryimmaculate.edu.ph" target="_blank">
+                            inquiries@collegeofmaryimmaculate.edu.ph</a>
+
+                    </p>
+                </div>
                 <div>
                     <button className = 'login-btn'>Login</button>
                 </div>
