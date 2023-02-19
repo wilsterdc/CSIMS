6966b7ade125ac92af0d3e216fc47ac30b1045f7
wilsterdc <delacruz.wilster15@gmail.com>
2023-02-19 22:12:06 +0800

    :rocket: initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,92 @@
+import {useState} from 'react'
 import './Login.css'
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:8000/students');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const users = await await response.json();
+            const user = users.find(
+                (u) => u.username === username && u.password === password
+            );
+
+            if (user) {
+                alert('Login successful!');
+            } else {
+                alert('Invalid username or password');
+            }
+        } catch (error) {
+            console.error('Login error! Username or Password is incorrect', error);
+            setError('Login failed! Username or Password is incorrect');
+        }
+        // try {
+        //     const response = await fetch('http://localhost:8000/student', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
+        //         body: JSON.stringify({username, password}),
+        //     });
+        //     const data = await response.json();
+        //     if (data.error) {
+        //         setError(data.error);
+        //     } else {
+        //         // setError('Login successful!');
+        //         localStorage.setItem('token', data.token);
+        //         window.location.href = '/protected-page'
+        //     }
+        // } catch (error) {
+        //     console.error('Login error! Username or Password is incorrect', error);
+        //     setError('Login failed! Username or Password is incorrect');
+        // }
+    };
+    
+
   return (
     <div>
         <div className = 'cover'>
             <h2>CSIMS</h2>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <div>
-                    <input className = 'login-input' type = 'text' placeholder = 'Username'></input>
+                    <input 
+                        className = 'login-input' 
+                        type = 'text' placeholder = 'Username'
+                        value = {username}
+                        onChange = {(e) => setUsername(e.target.value)}
+                        ></input>
                 </div>
                 <div>
-                    <input className = 'login-input' type = 'password' placeholder = 'Password'></input>
+                    <input 
+                        className = 'login-input' 
+                        type = 'password' placeholder = 'Password'
+                        value = {password}
+                        onChange = {(e) => setPassword(e.target.value)}
+                        ></input>
                 </div>
                 <div>
-                    <p id = 'failed'>Login failed! Incorrect username or password.</p>
+                    {error && <p id = 'failed'>{error}</p>}
                 </div>
                 <div>
-                    <p className = 'reset'>Forgot password? <br />Please contact the Office of the Registrar 
+                    <p className = 'reset'>
+                        Forgot password? <br />
+                        Please contact the Office of the Registrar 
                         through the following contact number: <strong><em>(044) 769 2021</em></strong> or
-                         through email; <a href = "mailto:inquiries@collegeofmaryimmaculate.edu.ph" target = 'blank'>
+                         through email; {' '} <a href = "mailto:inquiries@collegeofmaryimmaculate.edu.ph" target = 'blank'>
                             inquiries@collegeofmaryimmaculate.edu.ph</a>
                     </p>
                 </div>
                 <div>
-                    <button className = 'login-btn'>Login</button>
+                    <button onClick = {handleSubmit} className = 'login-btn'>Login</button>
                 </div>
             </form>
         </div>
